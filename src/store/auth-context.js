5a98110ce5c77645de545e94ddf19881cfaf11b5
eeyfesubalew <esubalew.seife@gmail.com>
2022-05-17 11:44:27 -0800

    auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -6,20 +6,32 @@ const AuthContext = React.createContext({
   login: (token) => {},
   logout: () => {},
 });
+
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationtime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationtime - currentTime;
+  return remainingDuration;
+};
+
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggenIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
-
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
